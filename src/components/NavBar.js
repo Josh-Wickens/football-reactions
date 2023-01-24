c570bedc63da54313115a31d07e32fa98a5eb0b7
Josh-Wickens <joshwickens@hotmail.com>
2023-01-24 10:45:19 +0000

    set current logged in users state so that nav bar will show elements if logged in or not.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo-fr.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const loggedInIcons = <>{currentUser?.username}</>
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        exact
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        Sign in
+      </NavLink>
+      <NavLink
+        exact
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signup"
+      >
+        Sign up
+      </NavLink>
+    </>
+  );
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container fluid>
-        <NavLink to="/"><Navbar.Brand>
-          <img src={logo} alt="logo" height="50" />
-        </Navbar.Brand></NavLink>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="50" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
               Home
             </NavLink>
-            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/signin">
-              Sign in
-            </NavLink>
-            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/signup">
-              Sign up
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
