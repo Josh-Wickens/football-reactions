5a5e83974567f462e61983cae4c553d60149c342
Josh-Wickens <joshwickens@hotmail.com>
2023-01-25 15:19:31 +0000

    add styles button to back button

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -6,18 +6,21 @@ import Container from "react-bootstrap/Container";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import { useHistory } from "react-router-dom";
 
 export const BackButton = () => {
-    let history = useHistory();
-    return (
-        <>
-          <button onClick={() => history.goBack()}><i class="fa fa-arrow-left" aria-hidden="true"></i>  Back</button>
-        </>
-    );
+  let history = useHistory();
+  return (
+    <>
+      <button className={btnStyles.Button, btnStyles.Blue} onClick={() => history.goBack()}>
+        <i class="fa fa-arrow-left" aria-hidden="true"></i> Back
+      </button>
+    </>
+  );
 };
 
 function PostPage() {
@@ -27,7 +30,6 @@ function PostPage() {
   const profile_image = currentUser?.profile_image;
   const support = currentUser?.support;
   const [comments, setComments] = useState({ results: [] });
-  
 
   useEffect(() => {
     const handleMount = async () => {
@@ -52,24 +54,23 @@ function PostPage() {
       </Col>
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        
+
         <Post {...post.results[0]} setPosts={setPost} postPage />
-        <BackButton className="primary align-center"/>
-        <Container className={appStyles.Content}>{currentUser ? (
-  <CommentCreateForm
-  profile_id={currentUser.profile_id}
-  profileImage={profile_image}
-  support={support}
-  post={id}
-  setPost={setPost}
-  setComments={setComments}
-  
-/>
-
-) : comments.results.length ? (
-  "Comments"
-) : null}</Container>
-
+        <BackButton className="primary" />
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              support={support}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+        </Container>
       </Col>
       <Col lg={2} sm={1} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
