cd94b78b8e28abbbc52286b8c171e56f1e14104f
Josh-Wickens <joshwickens@hotmail.com>
2023-01-26 10:50:54 +0000

    add the follow functionality for users to follow other users

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -5,13 +5,16 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { Button } from "react-bootstrap";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 const Profile = (props) => {
   const { profile, mobile, imageSize = 55 } = props;
-  const { id, following_id, image, owner } = profile;
+  const { id, following_id, image, owner, fan_or_club, support, official } = profile;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const {handleFollow } = useSetProfileData();
 
   return (
     <div
@@ -39,7 +42,7 @@ const Profile = (props) => {
           ) : (
             <Button
               className={`${btnStyles.Button} ${btnStyles.Black}`}
-              onClick={() => {}}
+              onClick={() => handleFollow(profile)}
             >
               follow
             </Button>
